2012e096d38a5f5b39abfa0a2280d3e35c95c591
khriz <khriz@pop-os.localdomain>
2023-02-27 22:42:07 -0800

    nuevo metodo ordenarIntercambio

--- Listaenlazadasimple/Src/lista_ordenada.js
+++ Listaenlazadasimple/Src/lista_ordenada.js
@@ -13,11 +13,11 @@ export default class ListaOrdenadaSimple extends ListaEnlazadaSimple{
 
     ordenarBurbuja(){
         
-        let copy = this.cabeza 
+        let copy = this.cabeza;
 
         while(copy !== null){  //mientras el enlace no sea null
 
-            let cat = copy.enlace
+            let cat = copy.enlace;
 
             while(cat !== null){   //cat sera el siguiente de copy
                 
@@ -35,4 +35,36 @@ export default class ListaOrdenadaSimple extends ListaEnlazadaSimple{
             copy = copy.enlace;
         }
     }
+
+    ordenarIntercambio(){
+        
+        let copy = this.cabeza;
+
+        while (copy != null) {
+
+            let cat = copy.enlace
+
+            while (cat != null) {
+
+                if (copy.data > cat.data) {
+
+                    let temp = copy.data
+                    copy.data = cat.data
+                    cat.data = temp;
+
+                    cat = cat.enlace;
+
+                }else{
+                        
+                    cat = cat.enlace;
+
+                }
+            }
+            copy = copy.enlace;
+        }
+    }   
+
+    
 }
+
+//crear un metodo para poder medir el tiempo al ejecutar ordenarburbuja
